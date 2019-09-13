c72dac244b6fba90c7835331f6bcaeafbeca098c
Hamid <h.gasmi@outlook.com>
2019-09-13 12:53:01 -0700

    RDS: MySQL + httpd

---
+++ EC2BootStrapScriptRDS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum install httpd php php-mysql -y
+cd /var/www/html
+wget https://wordpress.org/wordpress-5.1.1.tar.gz
+tar -xzf wordpress-5.1.1.tar.gz
+cp -r wordpress/* /var/www/html/
+rm -rf wordpress
+rm -rf wordpress-5.1.1.tar.gz
+chmod -R 755 wp-content
+chown -R apache:apache wp-content
+service httpd start
+chkconfig httpd on
