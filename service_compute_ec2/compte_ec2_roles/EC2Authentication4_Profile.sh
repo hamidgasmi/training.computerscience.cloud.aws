dcedd17c436dc2e8d50c75a705c116fb7f88e812
Hamid <mamid1706@hotmail.fr>
2019-11-03 14:17:11 -0800

    AWS CLI credential order: added profile credentials.

--- service_compute_ec2/compte_ec2_roles/EC2Authentication4_Profile.sh
+++ service_compute_ec2/compte_ec2_roles/EC2Authentication4_Profile.sh
@@ -1,5 +1,5 @@
 #Create a profile with AWS Access Key ID and Secret Access Key.
-aws configure --profile mamidDev
+aws configure --profile myProfileNonDefault
 #... AWS Access Key ID [None]: AKIAR5TTEBC52524V3ZV
 #... AWS Secret Access Key [None]: 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
 #... Default region name [None]: us-east-1
@@ -8,4 +8,4 @@ aws configure --profile mamidDev
 #... Environment variables should be returned.
 aws sts get-caller-identity
 #Test the profile: run aws sts commands with the profile create above
-aws sts get-caller-identity --profile mamidDev
+aws sts get-caller-identity --profile myProfileNonDefault
